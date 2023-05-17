8928ae9fa6a1813a2fe17898f6bb2802adca229d
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-05-17 10:50:20 +0200

    Fix Composer & PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,15 +2,6 @@ module.exports = {
 	plugins: [
 		['postcss-import'],
 		[
-			'@csstools/postcss-global-data',
-			{
-				files: [
-					'./theme/src/css/vars.css',
-					'./theme/src/css/vars/_media-queries.css',
-				],
-			},
-		],
-		[
 			'postcss-preset-env',
 			{
 				stage: 1,
