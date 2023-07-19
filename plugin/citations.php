10f51f26e8cd40f0811e930476a65dc16def6520
Luehrsen <gh-actions-Luehrsen@github.com>
2023-07-19 12:13:49 +0000

    Tagging 0.0.26

--- plugin/citations.php
+++ plugin/citations.php
@@ -9,7 +9,7 @@
  * Description: This WordPress Plugin introduces advanced citation capabilities to the WordPress Block Editor.
  * Author: WP Munich
  * Author URI: https://www.wp-munich.com
- * Version: 0.0.25
+ * Version: 0.0.26
  * Text Domain: citations
  */
 
