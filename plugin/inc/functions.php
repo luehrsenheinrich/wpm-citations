970940340548a81c55fdb6f4a838a4eab596cabd
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-08-11 16:27:54 +0200

    Fix linting issues

--- plugin/inc/functions.php
+++ plugin/inc/functions.php
@@ -24,52 +24,62 @@ function lh_plugin() {
 	return $plugin;
 }
 
+// Ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase for this function.
+// phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase, WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid
+
 /**
  * A php implementation of the classNames function from the classnames npm package.
  *
  * @return string The generated class names.
  */
-function classNames()
-{
+function classNames() {
 	$args = func_get_args();
 
-	$data = array_reduce($args, function ($carry, $arg) {
-		if (is_array($arg)) {
-			return array_merge($carry, $arg);
-		}
+	$data = array_reduce(
+		$args,
+		function ( $carry, $arg ) {
+			if ( is_array( $arg ) ) {
+				return array_merge( $carry, $arg );
+			}
 
-		$carry[] = $arg;
-		return $carry;
-	}, []);
+			$carry[] = $arg;
+			return $carry;
+		},
+		array()
+	);
 
-	$classes = array_map(function ($key, $value) {
-		$condition = $value;
-		$return = $key;
+	$classes = array_map(
+		function ( $key, $value ) {
+			$condition = $value;
+			$return    = $key;
 
-		if (is_int($key)) {
-			$condition = null;
-			$return = $value;
-		}
+			if ( is_int( $key ) ) {
+				$condition = null;
+				$return    = $value;
+			}
 
-		$isArray = is_array($return);
-		$isObject = is_object($return);
-		$isStringableType = !$isArray && !$isObject;
+			$isArray          = is_array( $return );
+			$isObject         = is_object( $return );
+			$isStringableType = ! $isArray && ! $isObject;
 
-		$isStringableObject = $isObject && method_exists($return, '__toString');
+			$isStringableObject = $isObject && method_exists( $return, '__toString' );
 
-		if (!$isStringableType && !$isStringableObject) {
-			return null;
-		}
+			if ( ! $isStringableType && ! $isStringableObject ) {
+				return null;
+			}
 
-		if ($condition === null) {
-			return $return;
-		}
+			if ( $condition === null ) {
+				return $return;
+			}
 
-		return $condition ? $return : null;
+			return $condition ? $return : null;
 
-	}, array_keys($data), array_values($data));
+		},
+		array_keys( $data ),
+		array_values( $data )
+	);
 
-	$classes = array_filter($classes);
+	$classes = array_filter( $classes );
 
-	return implode(' ', $classes);
+	return implode( ' ', $classes );
 }
